7893a841513639a40dd12fc5f33b0139e174c88a
sakshiborade123 <146450554+sakshiborade123@users.noreply.github.com>
2024-08-18 17:09:38 +0530

    Create Success.aspx.cs

---
+++ Ecommerce-ASP/Success.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Ecommerce
+{
+    public partial class Success : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Page Load event - no specific logic required here
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Redirect to the Dashboard page when Button1 is clicked
+            Response.Redirect("Dashboard.aspx");
+        }
+    }
+}
